f6275897475686b344f7d9f2d1bd37c99fd47d38
tfiorin <tfiorin@gmail.com>
2026-01-02 16:23:52 +1100

    updates

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-config" content="/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <body className={inter.className}>
         <LanguageProvider>
           <CartProvider>
